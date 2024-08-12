42e962e4e69f33c8f1cdeb4a78e2e327bd98887d
Armindo Junior <armindo.junior05@gmail.com>
2024-08-12 15:58:54 -0300

    feat: improve imports on index file

--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@ import * as locales from './locale/index.js';
 import * as imports from './imports.js';
 import * as constants from './constants.js';
 
+// Transform a string to camelCase
+const camelize = str =>
+  str
+    .replace(/(?:^\w|[A-Z]|\b\w)/g, (ltr, idx) => (idx === 0 ? ltr.toLowerCase() : ltr.toUpperCase()))
+    .replace(/\s+/g, '');
+
 /**
  *
  * @namespace pure
@@ -12,44 +18,15 @@ export default class Pure {
     this.possibleLocales = constants.possibleLocales;
 
     if (locale) {
-      if (this.possibleLocales.indexOf(locale) === -1) {
-        throw new Error(`The following locale is not supported: ${locale}`);
-      } else {
-        this.registeredModules = {
-          ...locales.en,
-          ...locales[locale]
-        };
-      }
+      this.setLocale(locale);
     } else {
       this.registeredModules = locales.en;
     }
 
-    this.random = new imports.Random(this);
-    this.fake = new imports.Fake(this);
-    this.unique = new imports.Unique(this);
-    this.helpers = new imports.Helpers(this);
-    this.name = new imports.Name(this);
-    this.address = new imports.Address(this);
-    this.company = new imports.Company(this);
-    this.finance = new imports.Finance(this);
-    this.image = new imports.Image(this);
-    this.lorem = new imports.Lorem(this);
-    this.hacker = new imports.Hacker(this);
-    this.internet = new imports.Internet(this);
-    this.database = new imports.Database(this);
-    this.phone = new imports.Phone(this);
-    this.date = new imports.Date(this);
-    this.commerce = new imports.Commerce(this);
-    this.system = new imports.System(this);
-    this.git = new imports.Git(this);
-    this.markdown = new imports.Markdown(this);
-    this.transport = new imports.Transport(this);
-    this.music = new imports.Music(this);
-    this.document = new imports.Document(this);
-    this.dessert = new imports.Dessert(this);
-    this.games = new imports.Games(this);
-    this.electricalComponents = new imports.ElectricalComponents(this);
-    this.esport = new imports.Esport(this);
+    // Dynamic import all modules
+    Object.keys(imports).forEach(key => {
+      this[camelize(key)] = new imports[key](this);
+    });
   }
 
   seed(value) {
@@ -58,10 +35,14 @@ export default class Pure {
   }
 
   setLocale(locale) {
-    this.registeredModules = {
-      ...locales.en,
-      ...locales[locale]
-    };
+    if (this.possibleLocales.indexOf(locale) === -1) {
+      throw new Error(`The following locale is not supported: ${locale}`);
+    } else {
+      this.registeredModules = {
+        ...locales.en,
+        ...locales[locale]
+      };
+    }
   }
 
   getSeed() {
--- test/locale.spec.js
+++ test/locale.spec.js
@@ -15,4 +15,17 @@ describe('Setting up all possible locales', () => {
       new Pure('XX');
     }).toThrow('The following locale is not supported: XX');
   });
+
+  pure.possibleLocales.forEach(locale => {
+    it(`setup ${locale} locale on setLocale method`, () => {
+      pure.setLocale(locale);
+      expect(pure.registeredModules.localeName).toEqual(locale);
+    });
+  });
+
+  it('setup invalid locale on setLocale method', () => {
+    expect(() => {
+      pure.setLocale('XX');
+    }).toThrow('The following locale is not supported: XX');
+  });
 });
